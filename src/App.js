d9ac124b12ce3439cf7cc80c89a4f0c2955390c1
ken sarowiwa <ken.wandolah@yahoo.com>
2022-06-23 06:17:17 +0300

    user creation form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './comps/Title';
+import UserForm from './comps/UserForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UserForm/>
     </div>
   );
 }
