85f756097aaeecd7e8817be7bd98e8a672c5bc44
ken sarowiwa <ken.wandolah@yahoo.com>
2022-06-25 22:17:01 +0300

    upload form

--- src/comps/UserForm.js
+++ src/comps/UserForm.js
@@ -1,8 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 const UserForm =()=>{
+
+    const[file, setFile] = useState(null);
+    const [error, SetError] = useState(null);
+    const allowed_Types = ['img/png', 'img/jpg']
+
+    function changeHandler(e){
+        var selected = e.target.files[0];
+        console.log(selected);
+
+        if (selected && allowed_Types.includes(selected.allowed_Types)){
+            setFile(selected)
+            SetError("")
+            
+        } else{
+            setFile(null);
+            
+            SetError("Kindly upload an image file with extensios png or jpg. Thank you")
+        }
+    
+    }
+
+    
     return(
         <div className="UserForm">
-            <input type="file"></input>
+            <input type="file" onChange={changeHandler} />
+                <div className="output">
+                    {error && <div className="error">{error}</div>}
+                    {file && <div className="filename">{file.name}</div>}
+                </div>
         </div>
     )
     
