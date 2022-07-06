6141982e03b67536955f67e2409daa82c650a696
ken sarowiwa <ken.wandolah@yahoo.com>
2022-07-06 23:25:00 +0300

    hooks

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,14 +1,24 @@
+
+
+
 //component to track the progress of a file upload, and display the progress in a progress bar. The component should accept a file object as a prop.
-import React from 'react'
+import React, { useEffect } from 'react'
 import useStorage from '../hooks/useStorage';
 
 export default function ProgressBar({file, setFile}) {
     const {url, progress} = useStorage(file);
-    console.log(url, progress)
+    console.log(url, progress);
+
+
+    useEffect(()=>{
+      if(url){
+        setFile(null);
+      }
+
+    },[url, setFile])
+
   return (
-    <div className='progress-bar'>
-        <h1>ProgressBar</h1>
-    </div>
+    <div className='progress-bar' style={{ width: progress + '%'}}></div>
   )
 }
 
