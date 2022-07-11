b0ebaccd02ee613e9a5980fd1126ab17bddc461b
ken sarowiwa <ken.wandolah@yahoo.com>
2022-07-11 19:33:07 +0300

    Updates

---
+++ precache-manifest.c4def3a5b0a2903c732d2063372ccca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443fb6afce8f7035a4b2eb0252cc9668",
+    "url": "/KenafricaGram/index.html"
+  },
+  {
+    "revision": "bf3a9fa9f655f2aa8417",
+    "url": "/KenafricaGram/static/css/main.3820e3cc.chunk.css"
+  },
+  {
+    "revision": "25998ea07262ba7dc42e",
+    "url": "/KenafricaGram/static/js/2.1d0a5cc0.chunk.js"
+  },
+  {
+    "revision": "02fdf8e254f630432b8320a9de05ddfb",
+    "url": "/KenafricaGram/static/js/2.1d0a5cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3a9fa9f655f2aa8417",
+    "url": "/KenafricaGram/static/js/main.b7d0a16d.chunk.js"
+  },
+  {
+    "revision": "0f32e9234bcc5bce0fa4",
+    "url": "/KenafricaGram/static/js/runtime-main.77193eab.js"
+  }
+]);
